18818bdb1a82365be9a5f7271ee1dfc5933dbbaf
Nabila Valinka <valinkanabila@gmail.com>
2021-03-03 16:17:44 +0700

    bela ok

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
-    public function index(){
+    public function index()
+    {
         $this->load->view('template/header');
         $this->load->view('admin/dashboard');
         $this->load->view('template/footer');
