e2744a34b7305b3e50213bc8b619816cb4655db0
isnamaku <isnamaku8@gmail.com>
2021-03-04 11:28:49 +0700

    beranda

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,8 +4,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Home extends CI_Controller
 {
     public function index(){
-        $this->load->view('template/header');
-        $this->load->view('admin/dashboard');
+        $this->load->view('beranda/header');
+        $this->load->view('beranda/index');
         $this->load->view('template/footer');
     }
 }
