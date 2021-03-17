b95a525b334634dbd21b8ad4f1cf163b41bc49d1
isnamaku <isnamaku8@gmail.com>
2021-03-17 15:56:21 +0700

    CRUD Barang Keluar dan Kontroler Berita Acara

--- application/views/admin/berita_acara.php
+++ application/views/admin/berita_acara.php
@@ -9,7 +9,7 @@
                 <div class="card shadow my-4 mb-4">
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table table-bordered" id="dataTable" width="200%" cellspacing="0">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                 <thead>
                                 <tr>
                                         <th>No</th>
--- application/views/admin/tambah_barang_keluar.php
+++ application/views/admin/tambah_barang_keluar.php
@@ -4,39 +4,55 @@
 <div class="container">
     <a style="color: #000000; font-weight:bold;">Tambah Barang Keluar</a>
     <hr>
-    <form action="<?= base_url('Logistik/prosesTambah'); ?>" method="POST">
+    <form action="<?= base_url('admin/proses_tambah_barang_keluar'); ?>" method="POST">
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
-                    <label style="color:#000000; font-weight:bold">Tanggal</label>
-                    <input type="date" style="background-color: #E5E5E5" class="form-control" name="tanggal" id="tanggal">
+                    <label style="color:#000000; font-weight:bold">Tanggal Keluar</label>
+                    <input type="date" style="background-color: #E5E5E5" class="form-control" name="tanggal_keluar" id="tanggal_keluar">
                 </div>
                 <div class="form-group">
-                    <label style="color:#000000; font-weight:bold">Kode Barang</label>
-                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="kode" id="kode">
+                    <label style="color:#000000; font-weight:bold">Sumber Pengirim</label>
+                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="sumber_2" id="sumber_2">
                 </div>
                 <div class="form-group">
-                    <label style="color:#000000; font-weight:bold">Barang</label>
-                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="barang" id="barang">
+                    <label style="color:#000000; font-weight:bold">Nama Penerima</label>
+                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="nama_penerima" id="nama_penerima">
                 </div>
                 <div class="form-group">
-                    <label style="color:#000000; font-weight:bold">Stock Masuk</label>
-                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="jumlah" id="jumlah">
+                    <label style="color:#000000; font-weight:bold">Jabatan Penerima</label>
+                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="jabatan_penerima" id="jabatan_penerima">
+                </div>
+                <div class="form-group">
+                    <label style="color:#000000; font-weight:bold">Instansi Penerima</label>
+                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="instansi_penerima" id="instansi_penerima">
                 </div>
             </div>
             <div class="col-md-6">
  
                 <div class="form-group">
-                    <label style="color:#000000; font-weight:bold">Nama Pengirim</label>
-                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="pengirim" id="pengirim">
+                    <label style="color:#000000; font-weight:bold">Kode Barang</label>
+                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="barcode" id="barcode">
                 </div>
                 <div class="form-group">
-                    <label style="color:#000000; font-weight:bold">Perusahaan Pengirim</label>
-                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="perusahaan" id="perusahaan">
+                    <label style="color:#000000; font-weight:bold">Barang</label>
+                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="nama_barang" id="nama_barang">
+                </div>
+                <div class="form-group">
+                    <label style="color:#000000; font-weight:bold">Jumlah Barang Keluar</label>
+                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="jumlah_keluar" id="jumlah_keluar">
+                </div>
+                <div class="form-group">
+                    <label style="color:#000000; font-weight:bold">Satuan</label>
+                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="satuan" id="satuan">
+                </div>
+                <div class="form-group">
+                    <label style="color:#000000; font-weight:bold">Keterangan</label>
+                    <input type="text" style="background-color: #E5E5E5" class="form-control" name="keterangan" id="keterangan">
                 </div>
             </div>
         </div>
         <button type="submit" class="btn" style="background-color: #FF8C00; color:#000000; font-weight:bold; float:right;">Tambah</button>
-        <a href="<?= base_url() ?>admin/barang_keluar" class="btn mr-3" style="background-color: #FF8C00; color:#000000; font-weight:bold; float:right;">Batal</a>
+        <a href="<?= base_url() ?>Admin/barang_keluar" class="btn mr-3" style="background-color: #FF8C00; color:#000000; font-weight:bold; float:right;">Batal</a>
     </form>
 </div>
