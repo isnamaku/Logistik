8f5f51ad2cf3047199e4d0fcd975aef732b2aa23
isnamaku <isnamaku8@gmail.com>
2021-03-23 17:15:39 +0700

    Drop down tambah barang masuk

--- application/views/admin/tambah_barang_masuk.php
+++ application/views/admin/tambah_barang_masuk.php
@@ -5,8 +5,57 @@
     <a style="color: #000000; font-weight:bold;">Tambah Barang Masuk</a>
     <hr>
     <form action="<?= base_url('admin/proses_tambah_barang_masuk'); ?>" method="POST">
+
+    <!-- Proses Panggi Barcode -->
+    <?php if (isset($_GET['test'])) {
+            $test = ($_GET['test']);
+
+            $last_row = $this->db->select('barcode')->order_by('barcode', "desc")->limit(1)->get('barang')->row();
+
+            $noUrut = (int)substr($last_row->barcode, 2, 6);
+            $noUrut++;
+
+            if ($test == '1') {
+                $str = '50';
+                $newKode = $str . sprintf('%04s', $noUrut);
+            } else {
+                $str = '51';
+                $newKode = $str . sprintf('%04s', $noUrut);
+            }
+        }
+        ?>
+
+        <!-- Akhir dariProses Panggi Barcode -->
+        <div class="card shadow my-4 mb-4">
+        <div class="card-body">
+            <form>
+                <div class="row">
+                    <div class="col-md-6">
+                        <p style="color:black;">Jenis Barang</p>
+                        <hr>
+                        <!-- Banyak Barang -->
+                        <iframe name="votar" style="display:none;"></iframe>
+                        <form action="" method="post" target="votar"> <a style="color:black;"> Pilih jenis barang yang akan dimasukkan : </a>
+
+                            <select name="test">
+                                <option value="1" name="1">Pangan</option>
+                                <option value="2" name="2">Non Pangan</option>
+                            </select>
+                            <br />
+                            <input type="submit" class="btn-primary " value="Lanjut" data-target="#sumbitModal" style="margin-top:20px;" />
+                        </form>
+                    </div>
+
+                </div>
+
+            </form>
+
+        <br>
+        <br>
+    
         <div class="row">
             <div class="col-md-6">
+
                 <div class="form-group">
                     <label style="color:#000000; font-weight:bold">Tanggal</label>
                     <input type="date" style="background-color: #E5E5E5" class="form-control" name="tanggal_masuk" id="tanggal_masuk">
